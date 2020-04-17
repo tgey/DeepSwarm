0bdef74bc51cd154968dc0ed4eb12a83811c8b8a
Thomas Gey <thomas.gey@outlook.fr>
2020-04-17 15:02:03 +0200

    gcloud script: dataset/setting_file bash args

--- gcloud_script.sh
+++ gcloud_script.sh
@@ -3,11 +3,16 @@
 INSTANCE_NAME=$(grep GCLOUD_INSTANCE_NAME .env | cut -d '=' -f2)
 ZONE_NAME=$(grep GCLOUD_INSTANCE_ZONE .env | cut -d '=' -f2)
 
+
+COMMAND="python3 $1.py -s $2"
+
 echo "START SCRIPT"
 
 echo $INSTANCE_NAME
 echo $ZONE_NAME
+echo "Dataset: $1 and setting file: $2"
 
+exit
 make clean
 echo "LOCAL: folders cleaned"
 
@@ -24,8 +29,11 @@ echo "VM: data transfered from local to instance"
 
 # gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'sudo apt install python3-pip'
 gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'pip3 install -r requirements-dev.txt'
-gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'ls -la'
-echo "VM: command executed"
+# gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command $COMMAND
+echo "VM: commands executed"
+
+# gcloud compute scp -recurse $INSTANCE_NAME:debug/ debug/
+# echo "VM: debug transfered from instance to local"
 
 gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE_NAME
 echo "VM: stopped"
