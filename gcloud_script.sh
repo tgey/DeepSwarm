6e0317c893dd68205acb4ec3c78c93a6ecfbd2b8
Thomas Gey <thomas.gey@outlook.fr>
2020-04-19 11:09:10 +0200

    PosixPath correction, gcloud script conditional on gcloud start

--- gcloud_script.sh
+++ gcloud_script.sh
@@ -4,7 +4,7 @@ INSTANCE_NAME=$(grep GCLOUD_INSTANCE_NAME .env.dev | cut -d '=' -f2)
 ZONE_NAME=$(grep GCLOUD_INSTANCE_ZONE .env.dev | cut -d '=' -f2)
 
 
-COMMAND="python3 $1.py -s $2"
+COMMAND="python3.6 $1 -s $2"
 
 echo "START SCRIPT"
 
@@ -16,26 +16,30 @@ make clean
 echo "LOCAL: folders cleaned"
 
 gcloud compute instances start $INSTANCE_NAME --zone=$ZONE_NAME
-echo "VM: started"
+returned=$(echo $?)
 
-gcloud compute scp --recurse deepswarm/ $INSTANCE_NAME:~
-gcloud compute scp --recurse examples/ $INSTANCE_NAME:~
-gcloud compute scp --recurse settings/ $INSTANCE_NAME:~
-gcloud compute scp --recurse tests/ $INSTANCE_NAME:~
-gcloud compute scp requirements-dev.txt $INSTANCE_NAME:~
-gcloud compute scp Makefile $INSTANCE_NAME:~
-echo "VM: data transfered from local to instance"
+if [[ $returned == 0 ]]
+then
+    echo "VM: started"
+    gcloud compute scp --recurse deepswarm/ $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    gcloud compute scp --recurse examples/ $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    gcloud compute scp --recurse settings/ $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    gcloud compute scp --recurse tests/ $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    gcloud compute scp requirements-dev.txt $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    gcloud compute scp Makefile $INSTANCE_NAME:~ --zone=$ZONE_NAME
+    echo "VM: data transfered from local to instance"
+    # gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'sudo apt install python3-pip'
+    # gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'python3.6 -m pip install -r requirements-dev.txt'
+    gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command "$COMMAND"
+    echo "VM: commands executed"
+    # gcloud compute scp -recurse $INSTANCE_NAME:saves/ gcloud_saves/
+    # echo "VM: debug transfered from instance to local"
+    gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE_NAME
+    echo "VM: stopped"
+    notify-send "DeepSwarm: gcloud VM run finished"
+    echo "END SCRIPT"
+else 
+    echo "VM: NOT started"
+    notify-send "Gcloud VM not started"
+fi
 
-# gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'sudo apt install python3-pip'
-gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command 'pip3 install -r requirements-dev.txt'
-# gcloud compute ssh --zone=$ZONE_NAME $INSTANCE_NAME --command $COMMAND
-echo "VM: commands executed"
-
-# gcloud compute scp -recurse $INSTANCE_NAME:debug/ debug/
-# echo "VM: debug transfered from instance to local"
-
-gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE_NAME
-echo "VM: stopped"
-
-notify-send "DeepSwarm: gcloud VM run finished"
-echo "END SCRIPT"
