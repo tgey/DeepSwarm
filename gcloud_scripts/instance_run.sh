7092e0e8121bce3e27efd359b020a13ff38709b3
Thomas Gey <thomas.gey@outlook.fr>
2020-05-15 18:22:33 +0200

    log update

--- gcloud_scripts/instance_run.sh
+++ gcloud_scripts/instance_run.sh
@@ -2,19 +2,19 @@
 
 python3.6 -m pip install -r requierements-dev-gpu.txt
 
-python3.6 $1 -s $2
+python3.6 $1 -s $2 &> log.txt
 python3.6 google_drive_sync.py
 
-python3.6 $1 -s $2
-python3.6 google_drive_sync.py
+# python3.6 $1 -s $2
+# python3.6 google_drive_sync.py
 
-python3.6 $1 -s $2
-python3.6 google_drive_sync.py
+# python3.6 $1 -s $2
+# python3.6 google_drive_sync.py
 
-python3.6 $1 -s $2
-python3.6 google_drive_sync.py
+# python3.6 $1 -s $2
+# python3.6 google_drive_sync.py
 
-python3.6 $1 -s $2
-python3.6 google_drive_sync.py
+# python3.6 $1 -s $2
+# python3.6 google_drive_sync.py
 
 sudo poweroff
